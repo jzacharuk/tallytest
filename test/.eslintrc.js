3c159fe55c40add53d176854b3736938831bed74
jzacharuk <jzacharuk@gmail.com>
2017-01-20 14:36:16 -0800

    Style fixes for testing eslint

--- test/.eslintrc.js
+++ test/.eslintrc.js
@@ -1,15 +1,11 @@
 module.exports = {
-    // "extends": "airbnb",
-    "installedESLint": true,
-    "env": {
-      "mocha": true,
-    },
-    "plugins": [
-        // "react",
-        // "jsx-a11y",
-        // "import"
-    ],
-    "rules": {
-      "import/no-extraneous-dependencies": ["error", {"devDependencies": ["**/*.spec.js"]}]
-    }
+  installedESLint: true,
+  env: {
+    mocha: true,
+  },
+  rules: {
+    'import/no-extraneous-dependencies': [ 'error', {
+      devDependencies: [ '**/*.spec.js' ],
+    }],
+  },
 };
